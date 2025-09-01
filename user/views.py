d392789a610d84b9fcfb6c99f5eef8ce0037a1b5
蒋雨宸 <23373059>
2025-09-01 19:36:12 +0800

    fix_UserInfoView

--- user/views.py
+++ user/views.py
@@ -368,14 +368,22 @@ class UserInfoView(ListCreateAPIView,RetrieveUpdateDestroyAPIView):
         #从请求头获取UUID,
         user = get_current_user(self.request)
         if not user:
-            return None
-        return User.objects.filter(user_id=user.id).first()
+            return Response({
+                "success": False,
+                "fail_code": "USER_NOT_FOUND",
+                "fail_msg": "用户不存在"
+            }, status=status.HTTP_404_NOT_FOUND)
+        return user
 
     def get_queryset(self):
         user = get_current_user(self.request)
         if not user:
-            return User.objects.none()
-        return User.objects.filter(user_id=user.id)
+            return Response({
+                "success": False,
+                "fail_code": "USER_NOT_FOUND",
+                "fail_msg": "用户不存在"
+            }, status=status.HTTP_404_NOT_FOUND)
+        return user
 
 
 class UploadAvatarView(APIView):
