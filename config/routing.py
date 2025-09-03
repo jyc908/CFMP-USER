77829d2ee2e3b05c9fc52b2d159ac9789ab8e817
Baidu <2698425919@qq.com>
2025-09-03 19:50:22 +0800

    WebSocket

--- config/routing.py
+++ config/routing.py
@@ -1,10 +1,11 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import routing
+from user.middleware import TokenAuthMiddleware
 
 application = ProtocolTypeRouter({
     # 其他协议处理器...
-    'websocket': AuthMiddlewareStack(
+    'websocket': TokenAuthMiddleware(
         URLRouter(
                 routing.websocket_urlpatterns
         )
